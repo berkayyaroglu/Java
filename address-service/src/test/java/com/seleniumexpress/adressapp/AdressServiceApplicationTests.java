9ccf1781e259d99181f164a6567b996113a33cac
DESKTOP-L23VP1P\berka <berkayyaroglu1@gmail.com>
2022-11-12 14:49:39 +0300

    Microservice Spring Boot

---
+++ address-service/src/test/java/com/seleniumexpress/adressapp/AdressServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seleniumexpress.adressapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdressServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
